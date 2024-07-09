359611e259bef54bc99f467fb9d4db9a30aecde2
Tikihead <64474914+Tikihead@users.noreply.github.com>
2024-07-09 12:23:29 -0700

    Delete SecLife php1/index.php

--- SecLife php1/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {    
-     $json = file_get_contents('php://input');
-     $data = json_decode($json);
-       foreach($data as $key=>$value){
-          echo $key . " : " . $value . "\n";
-        }
-  }	 
-?>
